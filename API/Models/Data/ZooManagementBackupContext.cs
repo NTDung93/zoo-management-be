2971d1422591132e10c890b4646a8bb95cc415b2
Long Vu <vulongbinhduong@gmail.com>
2023-10-10 09:48:34 +0700

    commit for database configuration

--- API/Models/Data/ZooManagementBackupContext.cs
+++ API/Models/Data/ZooManagementBackupContext.cs
@@ -1,16 +1,51 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using API.Models.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Models.Data
 {
     public class ZooManagementBackupContext : DbContext
     {
         public ZooManagementBackupContext()
-        {
-            
+        {    
         }
 
         public ZooManagementBackupContext(DbContextOptions<ZooManagementBackupContext> options) : base(options)
         {
         }
+
+        public DbSet<Animal> Animals { get; set; }
+        public DbSet<AnimalSpecies> AnimalSpecies { get; set; }
+        public DbSet<Area> Areas { get; set; }
+        public DbSet<Cage> Cages { get; set; }
+        public DbSet<Certificate> Certificates { get; set; }
+        public DbSet<EmployeeCertificate> EmployeeCertificates { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<FeedingSchedule> FeedingSchedules { get; set; }
+        public DbSet<Food> Foods { get; set; }
+        public DbSet<ImportHistory> ImportHistories { get; set; }
+        public DbSet<News> News { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Ticket> Tickets { get; set; }
+        public DbSet<TransactionHistory> TransactionHistories { get; set; }
+
+        private string GetConnectionString()
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", true, true).Build();
+            return configuration.GetConnectionString("DefaultConnection");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(GetConnectionString());
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+
     }
 }
