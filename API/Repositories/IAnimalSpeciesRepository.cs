eea8f137ba7af4814b7ad58650a2154461f4dc9a
Ntd93 <nguyentiendung2003@gmail.com>
2023-10-11 17:15:42 +0700

    hotfix interface animal species repository

--- API/Repositories/IAnimalSpeciesRepository.cs
+++ API/Repositories/IAnimalSpeciesRepository.cs
@@ -10,14 +10,9 @@ namespace API.Repositories
         Task<AnimalSpecies> GetAnimalSpecies(int id);
         Task<AnimalSpecies> GetAnimalSpecies(string name);
         Task<IEnumerable<AnimalSpecies>> GetAnimalSpeciesByName(string name);
-        Task<IEnumerable<AnimalSpecies>> GetSpecies();
-        Task<AnimalSpecies> GetSpeciesById(int id);
-        Task<IEnumerable<AnimalSpecies>> GetSpeciesByName(string name);
         Task<bool> CreateAnimalSpecies(AnimalSpecies species);
         Task<bool> UpdateAnimalSpecies(AnimalSpeciesResponse species);
         Task<bool> DeleteAnimalSpecies(int id);
-        Task<bool> UpdateSpecies(AnimalSpeciesDto species);
-        Task<bool> HasSpecies(int id);
         Task<bool> Save();
     }
 }
